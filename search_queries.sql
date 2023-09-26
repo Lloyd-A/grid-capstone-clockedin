91ea3b77ea0703a3ba263599c58e77f49b339952
lallen <lallen@griddynamics.com>
2023-09-26 17:40:51 -0500

    Completed Domain SQL query bullets 2,3,4,5 from Module

--- search_queries.sql
+++ search_queries.sql
@@ -1,14 +1,44 @@
 -- Search query with dynamic filters, pagination and sorting
--- Shift By Course
 
-SELECT * FROM shifts s JOIN locations l ON s.location_id = l.location_id
-    WHERE (s.course_enum = :course_enum OR :course_enum IS NULL)
-    AND (s.start_time = :start_time OR :start_time IS NULL)
-    AND (s.end_time = :end_time OR :end_time IS NULL)
-    AND (l.room_name = :room_name OR :room_name IS NULL)
-    AND (l.virtual_link = :virtual_link OR :virtual_link IS NULL);
+-- 1) Function that enables dynamic queries
 
+CREATE OR REPLACE FUNCTION dynamic_search(
+    column_name TEXT,
+   	filter_value text,
+   	order_by_criteria text,
+   	page_size INT
+)
+returns table (result_column UUID, start_time TIMESTAMP, end_time TIMESTAMP, location_id UUID, course_enum VARCHAR) as $$
+BEGIN
+	return QUERY execute
+		format('SELECT * FROM shifts WHERE %I = %L ORDER BY %I LIMIT %L',
+	column_name, filter_value, order_by_criteria, page_size);
+end;
+$$ language plpgsql
 
- CREATE OR REPLACE FUNCTION dynamic_search(
+-- SQL query to insert arguments into dynamic query: searching for shift_id, sorting by course and pagination of size 10
 
- )
+select * from dynamic_search('shift_id', '123e4567-e89b-12d3-a456-426655440000', 'course_enum', 10);
+
+-- 2) Search query with joined data for your use-cases
+
+-- finds labtechs' names for a shift with id 'a7d74f85-62e6-4c18-9df7-3b91f0a2d6e8'
+select first_name, last_name from users u join
+shifts_labtechs sl on u.user_id = sl.user_id join
+shifts s on sl.shift_id = s.shift_id
+where sl.shift_id = 'a7d74f85-62e6-4c18-9df7-3b91f0a2d6e8';
+
+-- 3) Statistic query; can be not related to your use-cases
+
+-- returns labtechs and number of shifts they have
+select first_name, last_name, count(sl.shift_id) from users u join
+shifts_labtechs sl on u.user_id = sl.user_id group by
+first_name, last_name;
+
+-- 4) Top-something query
+
+-- returns labtechs and number of shifts they have in descending order
+select first_name, last_name, count(sl.shift_id) as counter from users u join
+shifts_labtechs sl on u.user_id = sl.user_id group by
+first_name, last_name order by
+counter DESC;
