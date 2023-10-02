daf7704c8988fba2b55a9e82b5d8009c52bc8860
graheed <graheed@gmail.com>
2023-10-02 13:30:44 -0500

    Added Use Case Queries to project

--- usecase_queries.sql
+++ usecase_queries.sql
@@ -70,7 +70,7 @@ SELECT RequestShiftSwap(
                '123e4567-e89b-12d3-a456-426655440000'
            );
 
-select * from abstract_requests;
+
 
 --Function created to view all labtechs working on a particular shift
 
@@ -108,3 +108,224 @@ $$ LANGUAGE plpgsql;
 
 SELECT *
 FROM ViewShiftLabtechs('123e4567-e89b-12d3-a456-426655440000');
+
+
+--Function to view User's requests (made by them)
+CREATE OR REPLACE FUNCTION ViewMyShiftRequests (
+    n_user_id UUID,
+    n_request_status VARCHAR(255)
+)
+    RETURNS TABLE (
+                      request_id UUID,
+                      requestor_id UUID,
+                      reason VARCHAR(255),
+                      request_status VARCHAR(255),
+                      time_created TIMESTAMP,
+                      time_resolved TIMESTAMP,
+                      shift_id UUID,
+                      start_time TIMESTAMP,
+                      end_time TIMESTAMP,
+                      location_id UUID,
+                      room_name VARCHAR(255),
+                      virtual_link VARCHAR(100)
+                  ) AS $$
+BEGIN
+    -- Check if the shift exists
+    IF EXISTS (
+        SELECT 1
+        FROM abstract_requests ar
+                 JOIN shift_swap_requests swr ON ar.request_id = swr.request_id
+        WHERE ar.requestor_id = n_user_id
+    ) THEN
+        RETURN QUERY
+            SELECT ar.request_id, ar.requestor_id, ar.reason, ar.request_status,
+                   ar.time_created, ar.time_resolved, s.shift_id, s.start_time,
+                   s.end_time, l.location_id, l.room_name, l.virtual_link
+            FROM abstract_requests ar
+                     JOIN shift_swap_requests swr ON ar.request_id = swr.request_id
+                     JOIN shifts s ON swr.requested_shift_id = s.shift_id
+                OR swr.proposed_shift_id = s.shift_id
+                     JOIN locations l ON s.location_id = l.location_id
+            WHERE ar.request_status = n_request_status;
+    ELSE
+        RETURN QUERY
+            SELECT NULL::UUID,
+                   NULL::UUID,
+                   NULL::VARCHAR(255),
+                   NULL::VARCHAR(255),
+                   NULL::TIMESTAMP,
+                   NULL::TIMESTAMP,
+                   NULL::UUID,
+                   NULL::TIMESTAMP,
+                   NULL::TIMESTAMP,
+                   NULL::UUID,
+                   NULL::VARCHAR(255),
+                   NULL::VARCHAR(100);
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+
+SELECT *
+FROM ViewMyShiftRequests('b5092fa6-1c29-4eb1-95c9-7a6d8ec501e5', 'PENDING');
+
+--Function to view User's requests (Ones they need to approve or decline)
+CREATE OR REPLACE FUNCTION ViewOtherShiftRequests (
+    n_user_id UUID,
+    n_request_status VARCHAR(255)
+)
+    RETURNS TABLE (
+                      request_id UUID,
+                      requestor_id UUID,
+                      reason VARCHAR(255),
+                      request_status VARCHAR(255),
+                      time_created TIMESTAMP,
+                      time_resolved TIMESTAMP,
+                      shift_id UUID,
+                      start_time TIMESTAMP,
+                      end_time TIMESTAMP,
+                      location_id UUID,
+                      room_name VARCHAR(255),
+                      virtual_link VARCHAR(100)
+                  ) AS $$
+BEGIN
+    -- Check if the shift exists
+    IF EXISTS (
+        SELECT 1
+        FROM request_approvers ra
+        WHERE ra.user_id = n_user_id
+    ) THEN
+        RETURN QUERY
+            SELECT ar.request_id, ar.requestor_id, ar.reason, ar.request_status,
+                   ar.time_created, ar.time_resolved, s.shift_id, s.start_time,
+                   s.end_time, l.location_id, l.room_name, l.virtual_link
+            FROM request_approvers ra
+                     JOIN abstract_requests ar ON ra.request_id = ar.request_id
+                     JOIN shift_swap_requests swr ON ar.request_id = swr.request_id
+                     JOIN shifts s ON swr.requested_shift_id = s.shift_id
+                OR swr.proposed_shift_id = s.shift_id
+                     JOIN locations l ON s.location_id = l.location_id
+            WHERE ar.request_status = n_request_status;
+    ELSE
+        RETURN QUERY
+            SELECT NULL::UUID,
+                   NULL::UUID,
+                   NULL::VARCHAR(255),
+                   NULL::VARCHAR(255),
+                   NULL::TIMESTAMP,
+                   NULL::TIMESTAMP,
+                   NULL::UUID,
+                   NULL::TIMESTAMP,
+                   NULL::TIMESTAMP,
+                   NULL::UUID,
+                   NULL::VARCHAR(255),
+                   NULL::VARCHAR(100);
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+
+SELECT *
+FROM ViewOtherShiftRequests('b5092fa6-1c29-4eb1-95c8-7a6d8ec501e5', 'PENDING');
+
+--Function created to request time off
+
+CREATE OR REPLACE FUNCTION RequestTimeOff(
+    n_request_id UUID,
+    requestor_id UUID,
+    reason VARCHAR(255),
+    request_status VARCHAR(255),
+    time_created TIMESTAMP,
+    time_resolved TIMESTAMP,
+    start_date TIMESTAMP,
+    end_date TIMESTAMP
+)
+    RETURNS TEXT AS $$
+DECLARE
+    message TEXT;
+BEGIN
+    -- Check if the request already exists
+    IF EXISTS (
+        SELECT 1
+        FROM abstract_requests ar
+        WHERE ar.request_id = n_request_id
+    ) THEN
+        message := 'A request with this ID already exists.';
+    ELSE
+        -- Insert the time off request
+
+        INSERT INTO abstract_requests (request_id, requestor_id, reason, request_status, time_created, time_resolved)
+        VALUES (n_request_id, requestor_id, reason, request_status, time_created, time_resolved);
+
+        INSERT INTO time_off_requests (request_id, start_date, end_date)
+        VALUES (n_request_id, start_date, end_date);
+
+        message := 'Time off request created successfully.';
+    END IF;
+
+    RETURN message;
+END;
+$$ LANGUAGE plpgsql;
+
+
+SELECT RequestTimeOff(
+               '123e4567-e89b-12d3-a456-426655440111',
+               'b5092fa6-1c29-4eb1-95c9-7a6d8ec501e5',
+               'My wife will be entering labour, and I need to be there.',
+               'PENDING',
+               '2023-09-22 10:00:00',
+               null,
+               '2023-09-22 10:00:00',
+               '2023-09-25 10:00:00'
+           );
+
+
+--Function to view Time OFF requests (Ones the labmanagers need to approve or decline)
+CREATE OR REPLACE FUNCTION ViewTimeOffRequests (
+
+    n_request_status VARCHAR(255)
+)
+    RETURNS TABLE (
+                      request_id UUID,
+                      requestor_id UUID,
+                      reason VARCHAR(255),
+                      request_status VARCHAR(255),
+                      time_created TIMESTAMP,
+                      time_resolved TIMESTAMP,
+                      start_date TIMESTAMP,
+                      end_date TIMESTAMP
+
+                  ) AS $$
+BEGIN
+    -- Check if the shift exists
+    IF EXISTS (
+        SELECT 1
+        FROM abstract_requests ab
+                 JOIN time_off_requests tor ON ab.request_id = tor.request_id
+        WHERE ab.request_status = n_request_status
+    ) THEN
+        RETURN QUERY
+            SELECT ar.request_id, ar.requestor_id, ar.reason, ar.request_status,
+                   ar.time_created, ar.time_resolved, tor.start_date, tor.end_date
+            FROM abstract_requests ar
+                     JOIN time_off_requests tor ON ar.request_id = tor.request_id
+            WHERE ar.request_status = n_request_status;
+    ELSE
+        RETURN QUERY
+            SELECT NULL::UUID,
+                   NULL::UUID,
+                   NULL::VARCHAR(255),
+                   NULL::VARCHAR(255),
+                   NULL::TIMESTAMP,
+                   NULL::TIMESTAMP,
+                   NULL::TIMESTAMP,
+                   NULL::TIMESTAMP;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+
+SELECT *
+FROM ViewTimeOffRequests('PENDING');
+
+
